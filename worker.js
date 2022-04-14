7303674cd290338e242e46fc846258a4aa67f212
Can Avci <canavci2016@gmail.com>
2022-04-14 22:12:56 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,10 @@
+
+onmessage = function (e) {
+  var workerResult = 'Result: ' + e.data[0] + e.data[1];
+  postMessage(workerResult);
+};
+
+
 function fibonacci(numMax) {
   for (var fibArray = [0, 1], i = 0, j = 1, k = 0; k < numMax; i = j, j = x, k++) {
     x = i + j;
@@ -9,6 +16,8 @@ function fibonacci(numMax) {
 
 onconnect = function (e) {
   var port = e.ports[0];
+
+  port.postMessage("your sharedworker port is : " + port);
 
   port.onmessage = function (e) {
     port.postMessage(fibonacci(e.data[0]));
